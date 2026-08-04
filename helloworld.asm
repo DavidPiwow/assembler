ed69198eda9ed3488cdbdd1d543175baccd95556
snh-roy <133825328+snh-roy@users.noreply.github.com>
2026-08-03 22:27:45 -0400

    test with a 'Hello World' program

---
+++ helloworld.asm
@@ -0,0 +1,9 @@
+.ORIG x3000 ; Program begins at x3000.
+
+LEA R0, prompt1 ; Semi-colons are used to create comments.
+PUTS ; This prints the string prompt1. It is an OS call.
+
+HALT
+
+prompt1 .STRINGZ "Hello World!"
+.END
